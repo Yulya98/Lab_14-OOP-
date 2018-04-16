e357fb7de042b357527644fe48bf7ec045f6fdae
Yulya <yulya.kukareko@mail.ru>
2018-04-16 19:27:13 +0300

    first commit

---
+++ Lab_14_OOP/PO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+namespace Lab_14_OOP
+{
+    [Serializable]
+    [DataContract]
+    public class PO
+    {
+        [DataMember]
+        public string govnoKoderName;
+
+        public PO(string name)
+        {
+            govnoKoderName = name;
+        }
+        public PO()
+        { }
+
+        OperatingSystem os = Environment.OSVersion;
+
+        public void getOfOpereatingSystem()
+        {
+            Console.WriteLine("Platform:" + os.Platform);
+            Console.WriteLine("Major: " + os.Version.Major);
+            Console.WriteLine("Minor: " + os.Version.Minor);
+        }
+    }
+}
